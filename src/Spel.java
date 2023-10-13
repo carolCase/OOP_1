8808a38ac395e201b0f1f29dcb50a5f9845929ea
carolCase <carolinazona7@gmail.com>
2023-10-13 10:18:54 +0200

    try and catch

--- src/Spel.java
+++ src/Spel.java
@@ -4,62 +4,55 @@ import java.util.Scanner;
 public class Spel {
     
 public static void main(String[] args) {
-    Scanner scanner=new Scanner(System.in);
+    Scanner scanner=new Scanner(System.in); //detta ska nås i hela main
     String svar="ja";
      int tries=0;
        
 
     while (svar.equals("ja")){
            Random random=new Random();
-           int randomNumber=random.nextInt(100)+1;
+           int randomNumber=random.nextInt(100)+1; //100+1 efetersom den börjar räkna från 0.
            int gissningsTal=0;
           
-      while (gissningsTal!=randomNumber)
+      while (gissningsTal!=randomNumber) //här i inre while loopen startar spelet
       {
-         System.out.println("slump numret är:"+randomNumber);
+      //slå på om du ska testa-->   System.out.println("slump numret är:"+randomNumber);
         System.out.println("Gissa ett tal mellan 1 till 100");
-        gissningsTal= scanner.nextInt();
-         tries++;
-        
+       
+    try {  //funkade jättebra att ha den i inner while loop
+            gissningsTal = scanner.nextInt();
+            tries++;
 
             if (gissningsTal==randomNumber){
                 System.out.println("Du gissade rätt!!!");
                 System.out.println("Du gissade rätt på "+ tries +" försök");
                 System.out.println("Vill du fortsätta spela?(ja/nej)");
                 svar=scanner.next();
-               
-            }
+                }
                else if (gissningsTal<randomNumber){
                 System.out.println("fel! talet är större");
-               }
+                }
                else if (gissningsTal>randomNumber){
                 System.out.println("nej! talet är mindre");
-             }
-               else if (!hasNextInt(gissningsTal)) {
-                System.out.println("Du kan bara skriva ett tal med siffror");
-                
-               }
+                }
+
+       }         
+       catch (java.util.InputMismatchException e) {
+             System.out.println("Du kan bara skriva ett tal med siffror");
+             scanner.next();
+              }
     
     
-    }
+    } //här stängs andra while loop
 
 
 
-    }
+    }   //här stängs första while loop
 
 System.out.println("Tack för den här gången");
 scanner.close();
 
 
 }
-
-private static boolean hasNextInt(int gissningsTal) {
-    return false;
-}
-
-
-
-
-
 
 }
